337391c6fa0f8c33d55a6d4517ba3685b34ae2bb
Aj connect <ajiriiviero2021@gmail.com>
2024-03-13 16:19:02 +0100

    add

--- book_store/storagedetails/models.py
+++ book_store/storagedetails/models.py
@@ -5,16 +5,58 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 # Create your models here.
+class Country(models.Model):  #many to many relation
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+    class Meta:
+        verbose_name_plural = "Countries Name"
+        db_table = "Author Published"
+
+    def __str__(self) -> str:
+        return f"{self.name}, Code: {self.code}."
+
+
+class Address(models.Model):   #one to one relation
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = "Address Entries"
+        db_table = "Author Address"
+
+    def __str__(self) -> str:
+        return f"{self.postal_code}, {self.street}, {self.city}."
+
+
+class Author(models.Model):   #one to many relation
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self) -> str:
+        return self.full_name()
+    
+    class Meta:
+        db_table = "Author Details"
+
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name='books') #related_name is to change relation name(book_set)
+    #author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+    published_countries = models.ManyToManyField(Country)
 
     class Meta:
-        db_table = "books_info"
+        db_table = "Author Books info"
 
     def get_absolute_url(self):
         return reverse("book-details", args=[self.slug])
