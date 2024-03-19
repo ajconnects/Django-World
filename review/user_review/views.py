aa71a3456d3deb3c8a95b2743fc7913f7faad811
Aj connect <ajiriiviero2021@gmail.com>
2024-03-19 23:14:41 +0100

    new

--- review/user_review/views.py
+++ review/user_review/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def review(request):
+    if request.method == 'POST':
+        entered_username = request.POST['username']
+        print(entered_username)
+        return HttpResponseRedirect("/thank-you")
+    
     return render(request, "user_review/review.html")
+
+def thank_you(request):
+    return render(request, "user_review/thank_you.html")
 
 def application(request):
     return render(request, "user_review/application.html")
