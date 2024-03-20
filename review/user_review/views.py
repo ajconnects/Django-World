6e62bd7226ac58e1b29da4e413b81ffcf9dfc063
Aj connect <ajiriiviero2021@gmail.com>
2024-03-20 23:08:55 +0100

    add

--- review/user_review/views.py
+++ review/user_review/views.py
@@ -5,10 +5,18 @@ from django.http import HttpResponseRedirect
 def review(request):
     if request.method == 'POST':
         entered_username = request.POST['username']
-        print(entered_username)
+        entered_password = request.POST['password']
+
+        if entered_username == "" or entered_password == "":
+            return render(request, "user_review/review.html",{
+                "has_error": True
+            })
+        print(entered_username, entered_password)
         return HttpResponseRedirect("/thank-you")
     
-    return render(request, "user_review/review.html")
+    return render(request, "user_review/review.html", {
+        "has_error": False
+    })
 
 def thank_you(request):
     return render(request, "user_review/thank_you.html")
